4dda627b66251a88ffcca0f5e3627ea46007f08f
BALAJI GUNA <58567028+balajiguna@users.noreply.github.com>
2022-02-19 13:27:43 +0530

    Ui Issue fixed.

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,58 +4,52 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
- <section id="tech">
-<SectionDivider divider />
-<SectionTitle>Technologies</SectionTitle>
-<SectionText>
-  I've worked with diffrent projects such as Android , IOS , And Machine Learning.
-  <br/>
-      <br />
- Known Languages : Python , Kotlin , Java , Reactjs , Swift , Next Js. 
-      <br />
-      <br />
- Known Tools And IDE's : Android Studio, Xcode , VSCode, Pycharm , Netbeans.     
-</SectionText>
-<List>
-  <ListItem>
-    <picture>
-    <DiReact size={"3rem"}/>
-    </picture>
-    <ListContainer>
-      <ListTitle>Front-End</ListTitle>
-      <ListParagraph>
-        Experieence with <br/>
-        Kolin and swift
-      </ListParagraph>
-    </ListContainer>
-  </ListItem>
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+    I've worked with diffrent projects such as Android , IOS , And Machine Learning.
+    </SectionText>
+    <List>
       <ListItem>
         <picture>
-        <DiFirebase size={"3rem"}/>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+          Experience with <br/>
+        Kolin and swift
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
         </picture>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
-            Experieence with <br />
+            Experience with <br />
             Django and Flask
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-        <DiZend size={"3rem"}/>
+          <DiZend size="3rem" />
         </picture>
         <ListContainer>
           <ListTitle>UI/UX</ListTitle>
           <ListParagraph>
-            Experieence with <br />
+            Experience with <br />
             tools like Adobe Xd
           </ListParagraph>
         </ListContainer>
       </ListItem>
-</List>
-<SectionDivider colorAlt/>
- </section>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
 );
 
 export default Technologies;
